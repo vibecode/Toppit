d539dc4fe5c1932460912daf765d2fab87cd281c
vibecode <rumashkin@gmail.com>
2017-01-16 21:02:47 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import List from './components/List';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+        <List />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <div>Hello Fuck You</div>
+    );
+  };
+
+}
+
+export default List;
